332a6a3846ab4b65653d7044b5cfb2966d1e612d
Natanael <97618069+Natanael-hash@users.noreply.github.com>
2023-09-11 12:56:47 +0300

    Add files via upload

---
+++ blind_auction.py
@@ -0,0 +1,41 @@
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+print(logo)
+
+blind_dictionary = {} 
+
+def find_highest_bidder(bidding_record):
+    highest_bidder = 0
+    for biddder in bidding_record:
+        bid_amount = bidding_record[biddder]
+        if bid_amount > highest_bidder:
+            highest_bidder = bid_amount
+            winner = biddder
+    print(f"The winner is {winner} with a bid of {bid_amount} dollars!")
+
+while True:
+    player_name = input(" What is your name? ")
+    bid = int(input("What is your bid? $"))
+
+    blind_dictionary[player_name] = bid
+    
+    biders = input("Are there any other biders? Type 'yes' or 'no' ")
+    if biders == 'yes':
+        continue
+        
+    else:
+        find_highest_bidder(blind_dictionary)
+        print("See you at the next time!")
+        break
+    
